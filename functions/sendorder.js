c82f4942ff36f15336314fa11db9d1de91943b8b
Neil Alino <nvq.alino@gmail.com>
2020-11-07 22:18:05 +0800

    fix sendorder function 2

--- functions/sendorder.js
+++ functions/sendorder.js
@@ -3,7 +3,7 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 exports.handler = async (event, context, callback) => {
   try {
     const doc = new GoogleSpreadsheet('1pbOCk3zU1qNHS1U9_l8Wn1cd3GsO-OMWc2kqc1J5f6U');
-    await doc.useServiceAccountAuth(require('./sheets-db-271603-355f4e4ff03a.json'));
+    await doc.useServiceAccountAuth(require('./sheets-db-271603-09379e0f6d45.json'));
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
 
